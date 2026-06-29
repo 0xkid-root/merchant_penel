0e07bddf1ce2e14aacf269182c963d3d888b7652
0xkid-root <prohackers698@gmail.com>
2026-06-29 12:05:58 +0530

    remove favicon icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,23 +20,7 @@ export const metadata: Metadata = {
   title: 'AtMoonPe - Merchant Panel',
   description: 'All Your Payouts. One Powerful Platform.',
   generator: 'AtMoonPe.com',
-  icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
-  },
+ 
 }
 
 export const viewport: Viewport = {
