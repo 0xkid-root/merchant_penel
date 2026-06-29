e2cb0f40a48ba7195ad96fc093ad5636becdb5cb
0xkid-root <prohackers698@gmail.com>
2026-06-29 14:07:04 +0530

    reset ui is done

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,5 +1,13 @@
-import ResetPasswordPage from '@/features/auth/pages/reset-password'
 
-export default function Page() {
-  return <ResetPasswordPage />
+
+
+import AuthLayout from '@/components/auth/AuthLayout'
+import ResetPasswordForm from '@/features/auth/pages/reset-password'
+
+export default function ResetPasswordPage() {
+  return (
+    <AuthLayout>
+      <ResetPasswordForm />
+    </AuthLayout>
+  )
 }
